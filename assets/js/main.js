68be216a41addc58798d59587f396118492dcb81
Hamid Talebi <mr.talebihamid@hotmail.com>
2021-10-19 22:43:24 +0330

    zamzam landing template

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+function imgSlider(item) {
+  document.querySelector(".zamzam").src = item;
+}
+function changeBgColor(color) {
+  const section = document.querySelector(".section");
+  section.style.background = color;
+}
