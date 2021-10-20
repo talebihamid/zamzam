3f30c4cb53430ce082a8c2257574e9becdeb3166
Hamid Talebi <mr.talebihamid@hotmail.com>
2021-10-20 20:42:40 +0330

    make this template responsive

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,3 +5,9 @@ function changeBgColor(color) {
   const section = document.querySelector(".section");
   section.style.background = color;
 }
+function toggleMenu() {
+  const toggle_menu = document.querySelector(".toggle_menu");
+  toggle_menu.classList.toggle("active");
+  const navigation = document.querySelector(".navigation");
+  navigation.classList.toggle("active");
+}
